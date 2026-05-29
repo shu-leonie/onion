b933a178a18ce9822bef12706de932a3748277ca
cosimella <87551558+cosimella@users.noreply.github.com>
2026-05-29 03:12:11 +0200

    fix: UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\SelectedOutfit;
 class UserController extends Controller
 {
-    public function updateOffset(Request $request, User $user)
+    public function updateOffset(Request $request)
     {
         $request->validate([
             'temperature_offset' => 'required|integer',
+            'outfit_ids' => 'array', 
         ]);
 
         try {
+            $user = auth()->user();
+            
             $user->update([
                 'temperature_offset' => $user->temperature_offset + $request->input('temperature_offset'),
             ]);
 
+            if ($request->has('outfit_ids')) {
+                SelectedOutfit::whereIn('id', $request->input('outfit_ids'))
+                    ->where('user_id', $user->id)
+                    ->update(['has_been_reviewed' => 1]);
+            }
+
             $status = 'success';
-            $message = 'Die Änderungen wurden erfolgreich gespeichert.';
-        } catch (Exception $e) {
+            $message = 'Vielen Dank für dein Feedback!';
+        } catch (\Exception $e) {
             $status = 'error';
             $message = 'Beim Speichern der Änderungen ist ein Fehler aufgetreten.';
         }
 
-        return redirect()->back()->with($status, $message);
+        return view('review-success', [
+            'offset' => $request->input('temperature_offset')
+        ]);
     }
 }
-
-
